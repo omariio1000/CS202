80c34ce978df1a0ad864291caf5595642eae462f
Omar Nassar <omariio1000@gmail.com>
2022-01-31 20:04:25 -0800

    working on program 2 headers

--- Program2/activities.h
+++ Program2/activities.h
@@ -16,13 +16,15 @@
 #include <vector>
 #include <string>
 
-class activity {
+class activity {//Base class for activities/questions
     public:
         activity();
         activity(char * title, std::string details);
-        virtual ~activity();
+        activity(const activity & activity);
+        virtual ~activity();//Virtual destructor because of virtual functions
 
-        virtual void display();
+        //Virtual functions eliminate need for dynamic cast
+        virtual void display() const;
         virtual int setData(char * title, std::string details);
 
     protected:
@@ -30,13 +32,14 @@ class activity {
         std::string details;
 };
 
-class preparation : public activity {
+class preparation : public activity {//Derived exam prep questions
     public:
         preparation();
         preparation(char * title, std::string details, int type, int difficulty, char * sample);
+        preparation(const preparation & prep);
         ~preparation();
 
-        void display();
+        void display() const;
         int setData(char * title, std::string details, int type, int difficulty, char * sample);
         
     private:
@@ -45,13 +48,29 @@ class preparation : public activity {
         char * sample;
 };
 
-class problems : public activity {
+class problems : public activity {//Derived practice problems
     public: 
         problems();
         problems(char * title, std::string details, std::string prototype, int difficulty); 
+
+        void display() const;
+        int setData(char * title, std::string details, std::string prototype, int difficulty);
+
+    private:
+        int difficulty;
+        std::string prototype;
 };
 
-class concepts : public activity {
+class concepts : public activity {//Derived concepts for future study
+    public:
+        concepts();
+        concepts(char * title, std::string details);
+
+        void display() const;
+        int setData(char * title, std::string details, int proficiency);
+
+    private:
+        int proficiency;
 };
 
 #endif
---
+++ Program2/data.h
@@ -0,0 +1,64 @@
+/* Omar Nassar
+ * Portland State Universiy CS202
+ * January 31, 2022
+ * Data structure class that has node and array of DLLs
+ */
+
+#ifndef data_h
+#define data_h
+
+#include <cstring>
+#include <cctype>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <string>
+
+#include "activities.h"
+
+class node {//DLL node class
+    public:
+        node();
+        node(activity * data);
+        node(const node & node);
+        ~node();
+        
+        activity* getData();
+        void setData(activity * data);
+
+        node *& getNext();
+        void setNext(node *& next);
+
+        node *& getPrevious();
+        void setPrevious(node *& previous);
+
+        void display() const;
+
+    private:
+        activity * data; //data being stored is an activity/question
+        node * next;
+        node * previous;
+};
+
+class ADLL {//Class for array of DLLs data structure
+    public:
+        ADLL(int types = 3);
+        ADLL(const ADLL & obj)
+        ~ADLL();
+
+        int insertData(node * inserting);
+        int remove(char * title);
+        void displayAll() const;
+        void displayType(int type) const;
+        int retrieve(char * title);
+        int removeAll();
+        int removeType(int type);
+
+    private:
+        node ** questions; //double pointer for array of pointers
+        int types; //different types of activities
+        
+        //Recursive Functions (yet to add)
+};
