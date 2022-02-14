bffa02b807ae9356f2c71afd7e31b47c259a8468
Omar Nassar <omariio1000@gmail.com>
2022-02-14 13:54:01 -0800

    started program 3

---
+++ Program3/olympics.h
@@ -0,0 +1,109 @@
+/* Omar Nassar
+ * Portland State University CS202
+ * February 22, 2022
+ * Olympic activities classes (base + derived)
+ */
+
+#ifndef olympics_h
+#define olympics_h 
+
+#include <cstring>
+#include <cctype>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <string>
+
+class olympics {//base class for olympic activities
+    public:
+        ~olympics();
+        int setData(char*, std::string, int, int, int[]);
+
+        olympics& operator = (const olympics&);
+        
+        bool operator == (const olympics&) const;
+        bool operator != (const olympics&) const;
+        //bool operator < (const olympics&) const;
+        //bool operator <= (const olympics&) const;
+        //bool operator > (const olympics&) const;
+        //bool operator >= (const olympics&) const;
+
+        friend ostream& operator << (ostream&, const olympics&);
+
+    protected:
+        olympics();
+        olympics(const olympics&);
+
+        char * name;
+        string nation;
+        int age;
+        int weight;
+        int height;
+        int medals[3];
+};
+
+class ski : public olympics {//derived class for skiing
+    public: 
+        ski();
+        ski(const ski&);
+
+        int setData(char*, std::string, int, int, int, int[], int);
+        int setSponsor(std::string);
+
+        ski& operator = (const ski&);
+        
+        bool operator == (const ski&) const;
+        bool operator != (const ski&) const;
+
+        friend ostream& operator << (ostream&, const ski&);
+
+    private:
+        int time;
+        std::string sponsor;
+};
+
+class board : public olympics {//derived class for snowboarding
+    public: 
+        board();
+        board(const ski&);
+
+        int setData(char*, std::string, int, int, int, int[], int);
+        int setSponsor(std::string);
+
+        board& operator = (const board&);
+
+        bool operator == (const board&) const;
+        bool operator != (const board&) const;
+
+        friend ostream& operator << (ostream&, const board&);
+
+    private:
+        int time;
+        std::string sponsor;
+
+};
+
+class hockey : public olympics {//derived class for hockey
+    public: 
+        hockey();
+        hockey(const hockey&);
+
+        int setData(char*, std::string, int, int, int, int[]);
+        int setTeam(std::string);
+
+        hockey& operator = (const hockey&);
+
+        bool operator == (const hockey&) const;
+        bool operator != (const hockey&) const;
+
+        friend ostream& operator << (ostream&, const hockey&);
+
+    private:
+        std::string team;
+        std::string position;
+
+};
+
+#endif olympics_h
